818e8aaed2de037f3ba869483b5439e8991b4768
Luis Miguel Ortiz Rozalén <luismiguel.ortiz.rozalen@alumnos.upm.es>
2020-05-25 19:08:28 +0200

    Generating sample layout #7

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display menu bar', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-zest-frontend app is running!');
+    expect(page.isMenuBarPresent());
   });
 
   afterEach(async () => {
---
+++ src/app/dashboard/components/sidebar/sidebar.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'sample-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  visibleSidebar1;
+  visibleSidebar2;
+  visibleSidebar3;
+  visibleSidebar4;
+  visibleSidebar5;
+}
